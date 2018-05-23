5a3e1f2c45c59b702ce75f33a85b108318fa7abb
BcRikko <s.shin1106@gmail.com>
2018-05-23 09:34:30 +0900

    :sparkles: タスクのリソース定義を追加

--- design/api.go
+++ design/api.go
@@ -3,6 +3,8 @@ package design
 // golint:should not use dot imports と怒られるので名前付きでimportする
 import (
 	dsl "github.com/goadesign/goa/design/apidsl"
+	// API Resources
+	_ "github.com/BcRikko/learning-goa/design/resources"
 )
 
 // APIの定義(http://localhost:8080/api)
---
+++ design/resources/task.go
@@ -0,0 +1,72 @@
+package resources
+
+import (
+	"github.com/BcRikko/learning-goa/design/mediatypes"
+	goa "github.com/goadesign/goa/design"
+	dsl "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = dsl.Resource("Tasks", func() {
+	DefaultMedia(mediatypes.Task)
+	BasePath("/tasks")
+
+	// GET http://localhost:8080/api/tasks
+	dsl.Action("list", func() {
+		dsl.Routing(dsl.GET(""))
+		dsl.Description("タスク一覧を取得する。")
+		dsl.Response(goa.OK, dsl.CollectionOf(mediatypes.Task))
+	})
+
+	// GET http://localhost:8080/api/tasks/:taskID
+	dsl.Action("show", func() {
+		dsl.Routing(dsl.GET("/:taskID"))
+		dsl.Description("指定IDのタスクを取得する。")
+		dsl.Params(func() {
+			dsl.Param("taskID", goa.Integer, "タスクID")
+		})
+		dsl.Response(goa.OK)
+		dsl.Response(goa.NotFound)
+		dsl.Response(goa.BadRequest, goa.ErrorMedia)
+	})
+
+	// POST http://localhost:8080/api/tasks
+	dsl.Action("create", func() {
+		dsl.Routing(dsl.POST(""))
+		dsl.Description("タスクを作成する。")
+		dsl.Payload(func() {
+			dsl.Member("title", goa.String, "タスクのタイトル")
+			dsl.Required("title")
+		})
+		dsl.Response(goa.Created, "/tasks/[0-9]+")
+		dsl.Response(goa.BadRequest, goa.ErrorMedia)
+	})
+
+	// PUT http://localhost:8080/api/tasks/:taskID
+	dsl.Action("update", func() {
+		dsl.Routing(dsl.POST("/:taskID"))
+		dsl.Description("指定IDのタスクを更新する。")
+		dsl.Params(func() {
+			dsl.Param("taskID", goa.Integer, "タスクID")
+		})
+		dsl.Payload(func() {
+			dsl.Member("title", goa.String, "タスクのタイトル")
+			dsl.Member("done", goa.Boolean, "タスクの完了状態")
+			dsl.Required("title", "done")
+		})
+		dsl.Response(goa.NoContent)
+		dsl.Response(goa.NotFound)
+		dsl.Response(goa.BadRequest, goa.ErrorMedia)
+	})
+
+	// DELETE http://localhost:8080/api/tasks/:taskID
+	dsl.Action("delete", func() {
+		dsl.Routing(dsl.DELETE("/:taskID"))
+		dsl.Description("指定IDのタスクを削除する。")
+		dsl.Params(func() {
+			dsl.Param("taskID", goa.Integer, "タスクID")
+		})
+		dsl.Response(goa.NoContent)
+		dsl.Response(goa.NotFound)
+		dsl.Response(goa.BadRequest, goa.ErrorMedia)
+	})
+})
