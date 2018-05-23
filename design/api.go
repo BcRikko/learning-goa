9e198704239e6cb0bf4f7edc8373232670815d98
BcRikko <s.shin1106@gmail.com>
2018-05-23 09:18:03 +0900

    :sparkles: APIの定義を追加

---
+++ design/api.go
@@ -0,0 +1,18 @@
+package design
+
+// golint:should not use dot imports と怒られるので名前付きでimportする
+import (
+	dsl "github.com/goadesign/goa/design/apidsl"
+)
+
+// APIの定義(http://localhost:8080/api)
+var _ = dsl.API("Task", func() {
+	dsl.Title("タスク管理API")
+	dsl.Description("タスク管理のAPIです。")
+	dsl.Version("0.1")
+	dsl.Scheme("http")
+	dsl.Host("localhost:8080")
+	dsl.BasePath("/api")
+	dsl.Consumes("application/json") // リクエストのメディアタイプ
+	dsl.Produces("application/json") // レスポンスのメディアタイプ
+})
